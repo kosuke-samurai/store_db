4809a56d3e2e58c0ea0a12732b8bde5d87f5ecf8
kosuke-samurai <103375028+kosuke-samurai@users.noreply.github.com>
2022-08-28 18:33:27 +0900

    ともだち制限

--- fiend_act.php
+++ fiend_act.php
@@ -18,11 +18,34 @@ $second_user_id = $_POST["second_user_id"];
 
 $pdo = connect_to_db();
 
-//password_hash($password, PASSWORD_DEFAULT);
+//ともだち重複確認
+$sql = 'SELECT COUNT(*) FROM friend_table WHERE (first_user_id=:first_user_id AND second_user_id=:second_user_id) OR (first_user_id=:second_user_id AND second_user_id=:first_user_id)';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':first_user_id', $first_user_id, PDO::PARAM_STR);
+$stmt->bindValue(':second_user_id', $second_user_id, PDO::PARAM_STR);
 
-//sql
 
-$sql = "INSERT INTO friend_table (friend_id, first_user_id, second_user_id, created_at) VALUES (NULL, :first_user_id, :second_user_id, now())";
+try {
+    $status = $stmt->execute();
+} catch (PDOException $e) {
+    echo json_encode(["sql error" => "{$e->getMessage()}"]);
+    exit();
+}
+
+$reserve_count = $stmt->fetchColumn();
+//var_dump($reserve_count);
+//exit();
+
+
+
+//
+if ($reserve_count != 0) {
+    exit("すでに友達です");
+} else {
+    $sql = "INSERT INTO friend_table (friend_id, first_user_id, second_user_id, created_at) VALUES (NULL, :first_user_id, :second_user_id, now())";
+}
+
+
 
 $stmt = $pdo->prepare($sql);
 
